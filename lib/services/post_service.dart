49615ecb4709aaf1b8d079166fca884a5b40efa8
rizky herdiansyah <141700940+rizkyher@users.noreply.github.com>
2025-07-16 16:42:10 +0700

    feat(post): create post service for CRUD operations

---
+++ lib/services/post_service.dart
@@ -0,0 +1,67 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../utils/secure_storage.dart';
+
+class PostService {
+  final baseUrl = 'https://your-api.com';
+
+  Future<List<dynamic>> fetchAllPosts() async {
+    final token = await SecureStorage.getToken();
+    final res = await http.get(
+      Uri.parse('$baseUrl/posts'),
+      headers: {'Authorization': 'Bearer $token'},
+    );
+    if (res.statusCode == 200) {
+      return jsonDecode(res.body);
+    } else {
+      throw Exception('Gagal memuat postingan');
+    }
+  }
+
+  Future<Map<String, dynamic>?> getPostDetail(int id) async {
+    final token = await SecureStorage.getToken();
+    final res = await http.get(
+      Uri.parse('$baseUrl/posts/$id'),
+      headers: {'Authorization': 'Bearer $token'},
+    );
+    if (res.statusCode == 200) {
+      return jsonDecode(res.body);
+    }
+    return null;
+  }
+
+  Future<bool> createPost(Map<String, dynamic> data) async {
+    final token = await SecureStorage.getToken();
+    final res = await http.post(
+      Uri.parse('$baseUrl/posts'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(data),
+    );
+    return res.statusCode == 201;
+  }
+
+  Future<bool> updatePost(int id, Map<String, dynamic> data) async {
+    final token = await SecureStorage.getToken();
+    final res = await http.put(
+      Uri.parse('$baseUrl/posts/$id'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(data),
+    );
+    return res.statusCode == 200;
+  }
+
+  Future<bool> deletePost(int id) async {
+    final token = await SecureStorage.getToken();
+    final res = await http.delete(
+      Uri.parse('$baseUrl/posts/$id'),
+      headers: {'Authorization': 'Bearer $token'},
+    );
+    return res.statusCode == 200;
+  }
+}
