2b0e04e85cd6b2f6b48d90af3229616ed0d1211a
rizky herdiansyah <141700940+rizkyher@users.noreply.github.com>
2025-08-23 14:01:44 +0700

    chore(deps): resolve dependency conflict
    - merapihkan dependency
    - upgrade share_plus to ^11.1.0
    - downgrade file_picker to ^8.0.7

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,12 +24,9 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.portal_si"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23       
-        targetSdk = flutter.targetSdkVersion
+        minSdk = flutter.minSdkVersion
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
